83b937406bd9a2ef0bda87ea9f484da2c389aa84
Lastjosue22 <rolando.urias@clustervalley.com>
2021-04-18 13:52:38 -0600

    commit para cambiar de carpeta

--- GitRepoEjemplo/GitRepoEjemplo/GitRepoEjemplo/App.xaml.cs
+++ GitRepoEjemplo/GitRepoEjemplo/GitRepoEjemplo/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GitRepoEjemplo.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace GitRepoEjemplo
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new StartPage();
         }
 
         protected override void OnStart()
